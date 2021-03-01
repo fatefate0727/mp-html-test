c6e77bc09e94bad2b283fa58998cd0f6ee441775
fatefate0727 <1798440295@qq.com>
2021-03-01 18:20:39 +0800

    测试代码

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+/**
+ * @file index.js
+ * @author swan
+ */
+Page({
+    data: {
+        content:`<div>
+        Test 文本 011
+        <ul>
+        Test 文本 012
+                <li>
+                Test 文本 013
+                    <p>
+                        Test 文本 014
+                    </p>
+                </li>
+                </ul>
+        </div>`
+    },
+})
