7ba46f1917a6d00c696e3ae5ae7f7e6a52fe6a4b
LiuYaoPeng <1798440295@qq.com>
2021-03-08 10:14:43 +0800

    更新html文本

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,366 @@
  */
 Page({
     data: {
-        content:`<div>
-        Test 文本 011
+        content:`<div class="qinxue_1v1_con">
+    <p>
+        <br>
+    </p>
+    <p style="text-align:center;">
+        <img src="http://image.soxsok.com/image/20201113/20201113085005_4357.jpg" alt="" style="width:100%;">
+    </p>
+    <div style="text-align:center;">
+        <br>
+    </div>
+    <p>
+        <br>
+    </p>
+    <div class="qinxue_1v1_con_2_1">
+        <h2>
+            <span style="color:#006600;">四重服务保障</span>解家长学生后顾之忧
+        </h2>
         <ul>
-        Test 文本 012
+            <li>
+                <p>
+                    先听课后交钱01
+                </p>
+            </li>
+            <li>
+                <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_07.png" alt="" style="width:100%;">
+            </li>
+            <li>
+                <span>0元试听，预约试听，</span> <span>满意后再付款</span>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <p>
+                    不满意换老师02
+                </p>
+            </li>
+            <li>
+                <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_07.png" alt="" style="width:100%;">
+            </li>
+            <li>
+                <span>可以换班型，挑选自己心仪</span> <span>的老师授课</span>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <p>
+                    上课时间灵活03
+                </p>
+            </li>
+            <li>
+                <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_07.png" alt="" style="width:100%;">
+            </li>
+            <li>
+                <span>灵活授课，老师根据学生时</span> <span>间规划授课</span>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <p>
+                    不想上退余费04
+                </p>
+            </li>
+            <li>
+                <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_07.png" alt="" style="width:100%;">
+            </li>
+            <li>
+                <span>课程不满意，支持退剩余课</span> <span>时费用</span>
+            </li>
+        </ul>
+        <div style="clear:both;">
+        </div>
+    </div>
+    <div class="qx_jpb_con_2_1">
+        <h2>
+            中高考复读冲刺<span>这些问题困扰着你？</span>
+        </h2>
+        <div>
+            <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/cc-mg.png" alt="" style="width:100%;">
+        </div>
+        <ul>
+            <li>
+                <p>
+                    01基础薄弱
+                </p>
+                <div>
+                </div>
+                <span>基础不扎实，缺乏兴趣</span>
+            </li>
+            <li>
+                <p>
+                    02
+                    方法欠缺
+                </p>
+                <div>
+                </div>
+                <span>难以举一反三应用解题</span>
+            </li>
+            <li>
+                <p>
+                    03 自律性差
+                </p>
+                <div>
+                </div>
+                <span>自我复习效率低下</span>
+            </li>
+            <li>
+                <p>
+                    04
+                    过分偏科
+                </p>
+                <div>
+                </div>
+                <span>某一科目长期停滞不前</span>
+            </li>
+            <li>
+                <p>
+                    05
+                    苦学无效
+                </p>
+                <div>
+                </div>
+                <span>死记硬背，不会理解消化</span>
+            </li>
+            <li>
+                <p>
+                    06
+                    弱势科目拉分
+                </p>
+                <div>
+                </div>
+                <span>需解决弱势科目低分状况</span>
+            </li>
+        </ul>
+    </div>
+    <div style="clear:both;">
+    </div>
+    <div class="qinxue_1v1_con_3">
+        <h2>
+            <span>针对学生常见问题，</span>提供中高考学习辅导方案
+        </h2>
+        <div class="qinxue_1v1_con_3_1">
+            <ul>
                 <li>
-                Test 文本 013
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_2.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    1对1冲刺辅导
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                    量身定制教育目标、教学计划、辅导方案和执行管理系统。
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_3.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    1对2/1对3互动教学
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                    课堂互动体验，激发自主学习力，模块化课程精讲，查漏补缺。
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_4.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    专项巩固课
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                    针对偏科，如语文写作弱、英语听说差、数理化生一些科目弱的学生专项辅导。
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_1.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    考前冲刺课
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                    考前集中巩固、梳理知识、围绕大纲、解析真题真卷、不断训练临场发挥。
+                </li>
+            </ul>
+        </div>
+        <div style="clear:both;">
+        </div>
+        <div>
+            <a href="/pages/school/enroll/enroll?schoolId=2154">点击咨询 了解更多</a>
+        </div>
+    </div>
+    <div class="qinxue_1v1_con_4">
+        <h2>
+            ACE完整课程体系<span>为什么可以帮助学习？</span>
+        </h2>
+        <div>
+            <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/zk1.png" alt="" style="width:100%;">
+        </div>
+    </div>
+    <div class="qinxue_1v1_con_4">
+        <h2>
+            掌握阶段学习方法<span>针对性辅导</span>
+        </h2>
+        <div>
+            <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/zg2.png" alt="" style="width:100%;">
+        </div>
+    </div>
+    <div class="qinxue_1v1_con_7">
+        <h2>
+            <span>ITS教学APP</span>，师生互动教学
+        </h2>
+        <div>
+        </div>
+        <p>
+            通过ITS授课+校区教师授课进行教学。 ITS覆盖教学，帮助教师和学生完成检测盲点、备课、记录、推题、分析等过程。
+        </p>
+        <div class="qinxue_1v1_con_7_1">
+            <div>
+                <span>01</span><span>互动教学工具</span>
+            </div>
+            <ul>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/app-mb1.png" alt="" style="width:100%;">
                     <p>
-                        Test 文本 014
+                        ITS教学系统
                     </p>
                 </li>
-                </ul>
-        </div>`
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/app-mb2.png" alt="" style="width:100%;">
+                    <p>
+                        老师在线授课
+                    </p>
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/app-mb3.png" alt="" style="width:100%;">
+                    <p>
+                        视频多次回看
+                    </p>
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/app-mb4.png" alt="" style="width:100%;">
+                    <p>
+                        学情数据留存
+                    </p>
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/app-mb5.png" alt="" style="width:100%;">
+                    <p>
+                        1+1双层授课
+                    </p>
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/app-mb6.png" alt="" style="width:100%;">
+                    <p>
+                        形成教学闭环
+                    </p>
+                </li>
+            </ul>
+        </div>
+        <div style="clear:both;">
+        </div>
+        <div style="clear:both;">
+        </div>
+    </div>
+    <div class="qinxue_1v1_con_9">
+        <h2>
+            无论您在哪里，<span> 秦学教育都会为您贴心服务！</span>
+        </h2>
+        <div class="qinxue_1v1_con_9_1">
+            <ul>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_42.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_43.png" alt=""
+                        style="width:100%;">
+                </li>
+            </ul>
+            <ol>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_39.jpg" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_41.jpg" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_45.jpg" alt=""
+                        style="width:100%;">
+                </li>
+            </ol>
+        </div>
+        <div style="clear:both;">
+        </div>
+    </div>
+    <div class="qinxue_1v1_con_b">
+        <h2>
+            现在预约试听课程，<span>还可享六大好礼</span>
+        </h2>
+        <div class="qinxue_1v1_con_b_1">
+            <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_49.png" alt="" style="width:100%;">
+        </div>
+        <div style="clear:both;">
+        </div>
+    </div>
+    <div class="qinxue_1v1_con_f">
+        <h2>
+            企业荣誉|实力认证
+        </h2>
+        <div class="qinxue_1v1_con_f_1">
+            <ul>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_50.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_51.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_52.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_53.png" alt=""
+                        style="width:100%;">
+                </li>
+                <li>
+                    <img src="http://zhuanti.soxsok.com/kecheng_img/qx_zgkc/qinxue_1v1_54.png" alt=""
+                        style="width:100%;">
+                </li>
+            </ul>
+        </div>
+        <div style="clear:both;">
+        </div>
+        <a href="/pages/school/enroll/enroll?schoolId=2154">点击咨询 了解更多</a>
+    </div>
+</div>`
     },
 })
